5f3128a8dd6becacc19cef8b4a1586ce856c7300
evjeny <evjeny228@gmail.com>
2019-01-18 12:40:11 +0500

    new long pool api and some post-fixes

--- weirdo_bot/main.py
+++ weirdo_bot/main.py
@@ -1,38 +1,34 @@
 ﻿from itertools import combinations
 import random
+import json
 
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 from skimage.measure import structural_similarity as ssim
 from skimage.io import imread
 from skimage.transform import resize
 
 
+def get_params(params_file):
+    with open(params_file, "r") as file:
+        data = json.loads(file.read())
+        token = data["token"]
+        group_id = data["group_id"]
+        return token, group_id
+
+
 def send_message(event, message):
     random_id = random.randint(1, 2 ** 32)
-    if event.from_user:
-        vk.messages.send(
-            user_id=event.user_id,
-            message=message,
-            random_id=random_id
-        )
-    elif event.from_chat:
-        vk.messages.send(
-            chat_id=event.chat_id,
-            message=message,
-            random_id=random_id
-        )
+    vk.messages.send(
+        peer_id=event.object.peer_id,
+        message=message,
+        random_id=random_id
+    )
 
 
 def get_attached_photo_urls(event):
-    message_id = event.message_id
-    messages = vk.messages.getById(
-        message_ids=[message_id],
-        group_id=event.user_id
-    )
-    message = messages["items"][0]
-    attachments = message["attachments"]
+    attachments = event.object.attachments
     urls = []
     for attach in attachments:
         if attach["type"] == "photo":
@@ -90,36 +86,32 @@ def resize_images_to_one_size(images):
     return res
 
 
-token = ""
-with open("token.txt") as token_file:
-    token = token_file.readline()
-if token == "":
-    exit(1)
+if __name__ == '__main__':
+    token, group_id = get_params("params.json")
 
-vk_session = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
+    vk_session = vk_api.VkApi(token=token)
+    vk = vk_session.get_api()
+    longpoll = VkBotLongPoll(vk_session, group_id=group_id)
 
-print("bot started")
+    print("bot started")
 
-for current_event in longpoll.listen():
-    if current_event.type == VkEventType.MESSAGE_NEW:
-        print(current_event.message_id, current_event.text)
-    if current_event.type == VkEventType.MESSAGE_NEW and current_event.to_me and current_event.text:
-        if current_event.text.startswith("сравни"):
-            urls = get_attached_photo_urls(current_event)
-            images = get_images_from_urls(urls)
+    for current_event in longpoll.listen():
+        if current_event.type == VkBotEventType.MESSAGE_NEW and current_event.object.text:
+            if current_event.object.text.startswith("сравни"):
 
-            if len(images) < 2:
-                send_message(current_event, "слишком мало картинок :с")
-                continue
+                urls = get_attached_photo_urls(current_event)
+                images = get_images_from_urls(urls)
 
-            images = resize_images_to_one_size(images)
-            result = []
-            for i_a, i_b in combinations([i for i in range(len(images))], 2):
-                val = ssim(images[i_a], images[i_b], multichannel=True) * 100
-                val = round(val, 2)
-                result.append("картинка {} сходна с картинкой {} на {}%".format(min(i_a, i_b) + 1, max(i_a, i_b) + 1, val))
-            res = "\n".join(result)
+                if len(images) < 2:
+                    send_message(current_event, "слишком мало картинок :с")
+                    continue
 
-            send_message(current_event, res)
+                images = resize_images_to_one_size(images)
+                result = []
+                for i_a, i_b in combinations([i for i in range(len(images))], 2):
+                    val = ssim(images[i_a], images[i_b], multichannel=True) * 100
+                    val = round(val, 2)
+                    result.append("картинка {} сходна с картинкой {} на {}%".format(min(i_a, i_b) + 1, max(i_a, i_b) + 1, val))
+                res = "\n".join(result)
+
+                send_message(current_event, res)
